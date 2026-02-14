9b066c2312d89e1413a39b961fde1101f1cf41e9
Student Shaswat Gautam <shaswat2016@gmail.com>
2026-02-14 21:08:55 +0530

    aaa

--- Frontend/src/assets/components/Dashboard.jsx
+++ Frontend/src/assets/components/Dashboard.jsx
@@ -66,16 +66,17 @@ const Dashboard = ({ onLogout }) => {
       accountType: selectedCard,
     };
 
-    const response = await fetch(
-      "https://inde-hpbc.onrender.com/api/bank/save",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(payload),
-      }
-    );
+  const response = await fetch(
+  "https://inde-hpbc.onrender.com/api/bank/save",
+  {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payload),
+  }
+);
+
 
     const data = await response.json();
 
