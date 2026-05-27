56f09134735bf845a0d812d4718ca22026d3d33b
Shaswat Gautam <shaswat2016@gmail.com>
2026-05-28 02:58:34 +0530

    Update UserRoutes.js

--- Backend/routes/UserRoutes.js
+++ Backend/routes/UserRoutes.js
@@ -4,6 +4,6 @@ import { signupUser, loginUser } from "../controllers/UserControllers.js";
 const router = express.Router();
 
 router.post("/signup", signupUser);
-router.post("/login", loginUser);
+router.post("/logi", loginUser);
 
 export default router;
