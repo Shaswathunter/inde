4e6a1c58e026678c2cfb25aee352e4da14658b8b
Student Shaswat Gautam <shaswat2016@gmail.com>
2026-05-30 16:29:10 +0530

    asdfA

--- Backend/middleware/expiryMiddleware.js
+++ Backend/middleware/expiryMiddleware.js
@@ -1,10 +1,8 @@
 const expiryMiddleware = (req, res, next) => {
-  const expiryDate = new Date(process.env.APP_EXPIRY);
-
-  if (new Date() > expiryDate) {
+  if (process.env.APP_ACTIVE !== "true") {
     return res.status(403).json({
       success: false,
-      message: "Application Expired",
+      message: "DEPLOYMENT_NOT_FOUND",
     });
   }
 
